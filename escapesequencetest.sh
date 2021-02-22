d6ebea69b62695e5e32647dacefae760d1da9eba
Yosuke Nishikata <yosuke.nishikata@gmail.com>
2021-02-22 14:55:16 +0900

    ex nihilo

---
+++ escapesequencetest.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+
+set -x 
+
+echo  -e '\e[31;6m hoge \e[0m'
+
+echo  -e '\e[31;5m hoge \e[0m'
+
+echo  -e '\e[31;4m hoge \e[0m'
+
+echo  -e '\e[31;3m hoge \e[0m'
+
+echo  -e '\e[31;2m hoge \e[0m'
+
+echo  -e '\e[31;1m hoge \e[0m'
+
+echo  -e '\e[31m hoge \e[0m'
+
+
+
+echo -e "\e[5m \U1f363 \e[0m"
+
